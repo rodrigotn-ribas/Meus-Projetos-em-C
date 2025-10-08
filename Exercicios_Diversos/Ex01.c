a42bd296077175aaee3248b405068a27f4be7962
Rodrigo <rodrigotn.ribas@gmail.com>
2025-10-08 15:40:09 -0300

    Media notas

---
+++ Exercicios_Diversos/Ex01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/*
+Média de notas
+Leia 5 notas (array de float) e calcule a média.
+Depois mostre as notas acima da média.
+*/
+int main(){
+	// Sem um array
+	/*
+	float notas, somador, media, f;
+	int i;
+	
+	printf("Digite 5 notas: ");
+	for(i=0;i<5;i++){
+		scanf("%f", &notas);
+		somador += notas;
+		media = somador/i;
+	}
+	printf("media: %.2f", media);
+	*/
+	
+	// Com um array 
+	float notas[5];
+	float soma = 0, media;
+	int i;
+	
+	printf("Digite 5 notas\n");
+	for(i=0;i<5;i++){
+		printf("Nota %d: ", i+1);
+		scanf("%f", &notas[i]);
+		soma += notas[i];
+	}
+	
+	media = soma/5;
+	
+	printf("Media do aluno: %.2f\n\n", media);
+	printf("Notas acima da media\n");
+	for(i=0;i<5;i++){
+		if(notas[i] > 6){
+			printf("Nota %d: %.2f\n", i+1, notas[i]);
+		}
+	}
+}
+
